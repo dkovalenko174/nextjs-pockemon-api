13f6501511eac0fa61f3de26539f3205a3505eff
Dmitriy <d.kovalenko174@gmail.com>
2025-03-24 22:08:16 +0300

    Edited nextConfig.ts v2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	basePath: '/nextjs-pockemon-api',
+	output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+	basePath: process.env.NODE_ENV === 'production' ? '/nextjs-pockemon-api' : '',
 	reactStrictMode: true,
 	images: {
 		unoptimized: true
