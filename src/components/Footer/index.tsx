86d0cf8ff725c1f4fe162d20723370f262d24ed8
Dmitriy <d.kovalenko174@gmail.com>
2025-03-24 21:20:40 +0300

    Init Project

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import style from './Footer.module.css'
+const Footer = () => {
+	return (
+		<footer className={`container ${style.footer}`}>
+			Created by &copy;dkova
+		</footer>
+	)
+}
+
+export default Footer
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import style from './Navigation.module.css'
+
+type NavigateItems = {
+	id: number
+	title: string
+	href: string
+}
+
+const mockDataNav: NavigateItems[] = [
+	{
+		id: 1,
+		title: 'Getting Started',
+		href: '/getting'
+	},
+	{
+		id: 2,
+		title: 'Guides',
+		href: '/guides'
+	}
+]
+const Navigation = () => {
+	return (
+		<nav className={style.root}>
+			<div className={`container ${style.menu}`}>
+				{mockDataNav?.map((item) => (
+					<Link key={item.id} href={item.href} className="link">
+						{item.title}
+					</Link>
+				))}
+			</div>
+		</nav>
+	)
+}
+
+export default Navigation
